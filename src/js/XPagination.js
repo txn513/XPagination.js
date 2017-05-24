2e12ad5a95d472d792165dfe426f1af3f0b30d6b
Xuning Tian <xtian513@gmail.com>
2017-05-24 17:09:00 +0800

    fix bugs

--- src/js/XPagination.js
+++ src/js/XPagination.js
@@ -1,34 +1,258 @@
 window.xpagination = (function(){
 	var xp = function(params){
 		this.numPerPage = 10;  //默认每页显示数量
-		this.pageNo = 1; //默认第一页
-		this.oContainer = params.el;
-		this.totalPageNo = params.totalPageNo||8; //初始默认样式共8页
-		this.fontShowNo = params.fontShowNo||8;
-		this.endShowNo = params.endShowNo || 2;
+		this.el = params.el;   //父级id
+		this.totalPageNo = 1; //初始默认样式共1页
+		this.fontShowNo = params.fontShowNo||8;  //显示数量
+		//this.endShowNo = params.endShowNo || 2;
+		// this.leftRightNo = 2; //左右显示页数
+		this.curPageNo = 1; //默认当前页为1
+		//this.callback = params.callback; //回掉函数
+		this.totalRecord = 0; //总记录数
+		this.xPaginationWrap = null;   //父级对象
 		this.init();
 	}
 	xp.prototype = {
 		init: function(){
+			this.xPaginationWrap = document.getElementById(this.el);
 			this.renderHtml();
+			
+			//this.clickEvent();
+			// this.selectInit();
+		},
+		update: function(totalPageNo,totalRecord,callback){
+			this.totalPageNo = totalPageNo;
+			this.totalRecord = totalRecord; 
+
+			this.renderHtml();
+			callback(this.curPageNo, this.numPerPage);//callback发送请求
 		},
 		renderHtml: function(){
-			var xPaginationWrap = document.getElementById(this.oContainer);
-			var xContainer = document.createElement('div');
-			xContainer.className = 'xp_container clearFix';
-			var tempDiv= '<div class="xp_single_page_btn xp_page_home_btn">首页</div><div class="xp_single_page_btn xp_page_prev_btn">&lt;</div>'
-			for(var i=0; i<this.totalPageNo; i++){
-				tempDiv +='<div class="xp_single_page_btn xp_page_num_btn">' + (i+1)+'</div>';
-			}
-			tempDiv +='<div class="xp_single_page_btn xp_page_next_btn">&gt;</div><div class="xp_single_page_btn xp_page_lastpage_btn">尾页</div>';
-			xContainer.innerHTML = tempDiv;
-			//console.log(xContainer);
-			xPaginationWrap.innerHTML = '';
-			xPaginationWrap.appendChild(xContainer);
+			this.setPos(this.curPageNo);
 		},
 		clickEvent: function(){
+			var numBtns = document.querySelectorAll('.xp_page_num_btn');
+			var oHomeBtn = document.querySelector('.xp_page_home_btn');
+			var oLastBtn = document.querySelector('.xp_page_lastpage_btn');
+			var oPrevBtn = document.querySelector('.xp_page_prev_btn');
+			var oNextBtn = document.querySelector('.xp_page_next_btn');
+			var oSubmit = this.xPaginationWrap.querySelector('.submit');
 
+			var _self = this;
+
+			//数字按钮点击事件
+			for(var i = 0; i<numBtns.length; i++){
+				numBtns[i].addEventListener('click',function(){
+					_self.curPageNo = this.dataset.pageno;
+					//console.log(_self.fontShowNo);
+					_self.setPos(_self.curPageNo);
+					
+				});
+			}
+			//首页按钮点击事件
+			oHomeBtn.addEventListener('click',function(){
+				_self.curPageNo = 1;
+				_self.setPos(_self.curPageNo);
+			});
+			//尾页按钮点击事件
+			oLastBtn.addEventListener('click',function(){
+				_self.curPageNo = _self.totalPageNo;
+				_self.setPos(_self.curPageNo);
+			});
+			//后退按钮点击事件
+			oPrevBtn.addEventListener('click',function(){
+				_self.curPageNo--;
+				if(_self.curPageNo <1){
+					_self.curPageNo = 1;
+				}
+				_self.setPos(_self.curPageNo);
+			});
+			//前进按钮点击事件
+			oNextBtn.addEventListener('click',function(){
+				console.log(_self.curPageNo);
+				_self.curPageNo++;
+				if(_self.curPageNo > _self.totalPageNo){
+					_self.curPageNo = _self.totalPageNo;
+				}
+				_self.setPos(_self.curPageNo);
+			});
+			//确定按钮点击事件
+			oSubmit.addEventListener('click',function(e){
+				var target  = e.currentTarget;
+				var value = parseInt(target.previousElementSibling.value);
+				if(value!=0 &&  value <= _self.totalPageNo){
+					_self.curPageNo = value;
+					_self.setPos(_self.curPageNo);
+				}
+				//console.log(target.previousElementSibling);
+			});
 		},
+		setPos: function(num){
+			var newNum = parseInt(num);
+			//var xPaginationWrap = document.getElementById(this.el);
+
+
+			var xContainer = document.createElement('div');
+			xContainer.className = 'xp_container clearFix';
+
+
+			var tempDiv	='<div class="xp_left fLeft">'+
+			'<div class="fLeft">每页</div>'+
+			'<div id="xSelect" class="xselect fLeft">'+
+			'<span>'+ this.numPerPage +'</span>'+
+			'<ul class="dropdown">'+
+			'<li>10</li>'+
+			'<li>20</li>'+
+			'<li>40</li>'+
+			'<li>100</li>'+
+			'</ul>'+
+			'</div>'+
+			'<div class="fLeft">条数据&nbsp;共<span class="total_record">'+ 
+			this.totalRecord +
+			'</span>条数据，共<span class="total_page">'+ 
+			this.totalPageNo +
+			'</span>页。</div>'+
+			'</div>';
+
+			tempDiv += '<div class="xp_right fLeft"><div class="xp_single_page_btn xp_page_home_btn">首页</div><div class="xp_single_page_btn xp_page_prev_btn">&lt;</div>';
+
+
+			function activeClass(index){
+				if((index+1) == newNum){
+					tempDiv += '<div class="xp_single_page_btn xp_page_num_btn current" data-pageno="'+ (i+1) +'">' + (i+1)+'</div>';
+				}
+				else {
+					tempDiv +='<div class="xp_single_page_btn xp_page_num_btn" data-pageno="'+ (i+1) +'">' + (i+1)+'</div>';
+				}
+				
+			}
+
+			if(this.fontShowNo+1>this.totalPageNo){
+				for(var i = 0; i<this.totalPageNo; i++){
+					activeClass(i);
+				}
+			}
+			else {
+				var leftNum = Math.ceil((this.fontShowNo+1-3)/2);
+				var rightNum = this.fontShowNo+1-3-leftNum;
+				if((newNum-leftNum)>1 && (newNum+rightNum)<this.totalPageNo){
+
+					tempDiv +='<div class="xp_single_page_btn xp_page_ellipsis_btn">...</div>';
+					for(var i = (newNum-leftNum-1); i<(newNum+rightNum); i++){
+						activeClass(i);
+					}
+					tempDiv +='<div class="xp_single_page_btn xp_page_ellipsis_btn">...</div>';
+				}
+				else if((newNum-leftNum) <=1){
+					for(var i = 0; i<this.fontShowNo; i++){
+						activeClass(i);
+					}
+					tempDiv +='<div class="xp_single_page_btn xp_page_ellipsis_btn">...</div>';
+				}
+				else if((newNum+rightNum)>=this.totalPageNo){
+
+					tempDiv +='<div class="xp_single_page_btn xp_page_ellipsis_btn">...</div>';
+					for(var i = (this.totalPageNo-this.fontShowNo) ; i<this.totalPageNo; i++){
+						activeClass(i);
+					}
+				}
+			}
+			
+			
+			//console.log(num);
+
+			
+
+			
+			tempDiv +='<div class="xp_single_page_btn xp_page_next_btn">&gt;</div><div class="xp_single_page_btn xp_page_lastpage_btn">尾页</div></div><div class="xp_redirect_part fLeft">到<input type="text">页<span class="submit">确定</span></div>';
+			xContainer.innerHTML = tempDiv;
+			this.xPaginationWrap.innerHTML = '';
+			this.xPaginationWrap.appendChild(xContainer);
+
+			this.clickEvent();
+			this.selectInit();
+			// this.callback(this.curPageNo, this.numPerPage);
+			
+		},
+		selectInit: function(){
+			var obj = this;
+            var oXSelect = document.getElementById('xSelect');
+            var oLis = oXSelect.querySelectorAll('li');
+            var oSpan = oXSelect.querySelector('span');
+
+            oXSelect.addEventListener('click',function(e){
+                e.stopPropagation();
+                var allSiblings = document.querySelectorAll('.xselect');
+               
+                
+                //console.log(obj.hasClass(this,'active'));
+                obj.toggleClass(this,'active'); 
+
+
+            });
+
+            for(var i=0; i<oLis.length; i++){
+                oLis[i].addEventListener('click', function(){
+                    obj.numPerPage = this.innerHTML;
+                    if(oSpan.innerHTML == obj.numPerPage) return;
+                    oSpan.innerHTML = obj.numPerPage;
+                    obj.removeClass(oSpan.parentNode,'active')
+                    obj.curPageNo = 1;  //还原到第一页
+					obj.setPos(obj.curPageNo);
+                });
+            }
+		},
+		hasClass: function(obj, cls) {
+            var obj_class = obj.className,//获取 class 内容.
+              obj_class_lst = obj_class.split(/\s+/);//通过split空字符将cls转换成数组.
+              x = 0;
+              for(x in obj_class_lst) {
+                if(obj_class_lst[x] == cls) {//循环数组, 判断是否包含cls
+                  return true;
+                }
+              }
+              return false;
+           // console.log(new RegExp('(\\s|^)' + cls + '(\\s|$)'))
+        },
+        removeClass: function(obj, cls) {
+            if (this.hasClass(obj, cls)) {
+                var obj_class = ' '+obj.className+' ';//获取 class 内容, 并在首尾各加一个空格. ex) 'abc    bcd' -> ' abc    bcd '
+                  obj_class = obj_class.replace(/(\s+)/gi, ' '),//将多余的空字符替换成一个空格. ex) ' abc    bcd ' -> ' abc bcd '
+                  removed = obj_class.replace(' '+cls+' ', ' ');//在原来的 class 替换掉首尾加了空格的 class. ex) ' abc bcd ' -> 'bcd '
+                  removed = removed.replace(/(^\s+)|(\s+$)/g, '');//去掉首尾空格. ex) 'bcd ' -> 'bcd'
+                  obj.className = removed;//替换原来的 class.
+            }
+        },
+        toggleClass: function(obj,cls){
+            //console.log(this.hasClass(obj,cls));
+            if(this.hasClass(obj,cls)){
+                //console.log(1);
+                this.removeClass(obj, cls);
+            } else {
+           // console.log(2);
+                this.addClass(obj, cls);
+            }
+        },
+        addClass: function(obj, cls) {
+            if (!this.hasClass(obj, cls)) {
+                obj.className += " " + cls;
+            }
+        },
+        siblingElems: function(elem){
+               	var nodes=[ ];
+                var _elem=elem;
+               	while((elem=elem.previousSibling)){
+                      if(elem.nodeType==1){
+                             nodes.push(elem);
+                      }
+               	}
+               	var elem=_elem;
+              	while((elem=elem.nextSibling)){
+                  	if(elem.nodeType==1){
+                       	nodes.push(elem);
+                  	}
+               	}
+        },
 	}
 	return xp;
 })();
